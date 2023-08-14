f7412ae20148f01ae21d313fe7447b868a724889
Jason <121495300+jason21715@users.noreply.github.com>
2023-08-14 04:50:27 -0700

    Add FAQ section/collapsibles and Add color scheme change

--- Frontend/src/Components/Dashboard.tsx
+++ Frontend/src/Components/Dashboard.tsx
@@ -1,28 +1,61 @@
 import React from "react";
 import "../Styles/Dashboard.scss";
+import { RiArrowDropDownLine } from "react-icons/ri";
+import {
+  AiOutlineSearch,
+  AiOutlinePushpin,
+  AiOutlineFolder,
+  AiOutlinePlus,
+  AiOutlineLink,
+} from "react-icons/ai";
+import { TfiLink } from "react-icons/tfi";
 
 function Dashboard() {
+  // SET REMINDERS/NOTIFICATIONS SO USER CAN COME BACK ON RIGHT DAY
+
   return (
     <main className="dashboard-container">
       <div className="d-sidebar">
-        <div className="sidebar-profile">Jason</div>
-        <input placeholder="Search links or folders"></input>
+        <div className="sidebar-profile">
+          <p>Jason</p>
+          <RiArrowDropDownLine size={25} />
+        </div>
+
+        <div className="sidebar-search">
+          <AiOutlineSearch />
+          <input id="search" placeholder="Search links or folders..."></input>
+        </div>
+
         <div className="sidebar-pins">
-          {" "}
+          <AiOutlinePushpin />
           <h1>Pinned</h1>
         </div>
+
         <div className="sidebar-folders">
-          <h1>Folders</h1>
-          <button>Create new folder</button>
+          <div className="folders-flex">
+            <AiOutlineFolder />
+            <h1>Folders</h1>
+          </div>
+
+          <button>
+            <AiOutlinePlus color="#f77a4a" size={20} />
+            Create new folder
+          </button>
         </div>
       </div>
 
       <div className="d-main">
-        <div className="main-title">Title</div>
+        <div className="main-title">Recent</div>
 
         <div className="main-functions">
-          <button>Add Link</button>
-          <button>Add Multiple</button>
+          <button>
+            <AiOutlineLink /> Add Link{" "}
+            <AiOutlinePlus color="#f77a4a" size={20} />{" "}
+          </button>
+          <button>
+            <TfiLink />
+            Add Multiple <AiOutlinePlus color="#f77a4a" size={20} />
+          </button>
         </div>
 
         <div className="main-content">
