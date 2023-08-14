49af0e4d995e7363e464d77eae6e008219513ffb
Jason <121495300+jason21715@users.noreply.github.com>
2023-08-14 01:18:09 -0700

    Add rebranding landing page

--- Frontend/src/Components/Home.tsx
+++ Frontend/src/Components/Home.tsx
@@ -1,21 +1,22 @@
 import React from "react";
 import "../Styles/Home.scss";
-import { BsClockHistory } from "react-icons/bs";
-import { HiUserGroup } from "react-icons/hi";
-import { AiOutlineCheck } from "react-icons/ai";
+import { AiOutlineCheck, AiFillPicture, AiFillFilter } from "react-icons/ai";
 
 function Home() {
   return (
     <main className="home-page">
       <nav>
         <ul className="nav-bar">
-          <li id="logo">CodePlanner</li>
+          <li id="logo">LinkSaver</li>
 
-          <div className="nav-right">
-            <a>Home</a>
+          <div className="nav-mid">
             <a>About</a>
             <a>Contact</a>
-            <a>Login</a>
+            <a>FAQ</a>
+          </div>
+
+          <div className="nav-right">
+            <a>Log in</a>
             <a id="getStarted">Get Started</a>
           </div>
         </ul>
@@ -23,87 +24,70 @@ function Home() {
 
       <header className="landing-page">
         <div className="landing-heading">
-          <section>
-            <strong>Creating</strong>, <strong>Planning</strong>, and{" "}
-            <strong>Building</strong> projects should be <i>easy</i>. Start
-            today with CodePlanner
-          </section>
-          <div id="placeholder"></div>
+          <section>Keep all your links in one place</section>
         </div>
+        <div id="placeholder"></div>
       </header>
 
-      <section className="landing-features">
-        <h1 className="landing-features-heading">Why Us?</h1>
-        <div className="landing-features-flex">
-          <div className="landing-features-item">
-            <BsClockHistory color={"#504eeb"} size={75} />
-            <p className="landing-features-item-heading">Save Time</p>
-            <p className="landing-features-item-text">
-              Our platform simplifies the project planning process, and
-              optimizes the workflow, allowing you to focus on coding and
-              developing
+      <section className="home-features">
+        <h1>Why Us?</h1>
+        <div className="home-features-flex">
+          <div className="home-features-item">
+            <AiFillFilter color={"#f77a4a"} size={75} />
+            <h2>Filtering</h2>
+            <p>
+              Quickly find your links. With a few clicks, you can locate your
+              links, making it simple.
             </p>
           </div>
-          <div className="landing-features-item">
-            <HiUserGroup color={"#504eeb"} size={75} />
-            <p className="landing-features-item-heading">Collaboration</p>
-            <p className="landing-features-item-text">
-              Work seamlessly with your team, share ideas, and track progress
-              together. Boosting productivity
+          <div className="home-features-item">
+            <AiFillPicture color={"#f77a4a"} size={75} />
+            <h2>Visualization</h2>
+            <p>
+              Every saved link comes with a thumbnail, allowing you to identify
+              your content at a glance.
             </p>
           </div>
-          <div className="landing-features-item">
-            <AiOutlineCheck color={"#504eeb"} size={75} />
-            <p className="landing-features-item-heading">Simplified</p>
-            <p className="landing-features-item-text">
-              Hassle-free project planning with our user-friendly interface,
-              making it easy to stay organized throughout development
+          <div className="home-features-item">
+            <AiOutlineCheck color={"#f77a4a"} size={75} />
+            <h2>Simplified</h2>
+            <p>
+              Hassle-free link saving with our user-friendly interface, making
+              it easy to stay organized
             </p>
           </div>
         </div>
       </section>
 
-      <section className="landing-tutorial">
-        <h1 className="landing-tutorial-heading">How it works</h1>
+      <section className="home-faq">
+        <h1>Frequently Asked Questions</h1>
 
-        <div className="landing-tutorial-container">
-          <div className="landing-tutorial-item">
-            <h2 className="landing-tutorial-item-head">Create a Project</h2>
-            <p style={{ width: "50%" }}>
-              Effortlessly create new coding projects with deadlines, and a tech
-              stack. No idea where to start? AI can help you.
-            </p>
-            <div className="vl"></div>
-          </div>
-
-          <div className="landing-tutorial-item">
-            <h2 className="landing-tutorial-item-head">Invite Users</h2>
-            <p style={{ width: "50%" }}>
-              Collaborate easily by inviting team members to join your project
-              and assigning roles to members.
-            </p>
-            <div className="vl"></div>
-          </div>
-
-          <div className="landing-tutorial-item">
-            <h2 className="landing-tutorial-item-head">Adding Tasks</h2>
-            <p style={{ width: "50%" }}>
-              Streamline task management. Add tasks and organize your coding
-              projects efficiently, ensuring a structured workflow and optimized
-              development process.
-            </p>
-            <div className="vl"></div>
-          </div>
-
-          <div className="landing-tutorial-item">
-            <h2 className="landing-tutorial-item-head">Finished Project</h2>
-          </div>
-        </div>
+        <div className="landing-tutorial-container"></div>
       </section>
 
-      <footer className="landing-footer">
-        <p className="landing-footer-note">Begin creating with CodePlanner</p>
-        <a className="landing-footer-button">Get Started for free</a>
+      <footer className="home-contact">
+        <h1>CONTACT</h1>
+        <form className="contact-form">
+          <div>
+            <label htmlFor="name">NAME</label>
+            <input type="text" id="name" name="name" required></input>
+          </div>
+
+          <div>
+            <label htmlFor="email">EMAIL</label>
+            <input type="email" id="email" name="email" required></input>
+          </div>
+
+          <div>
+            <label htmlFor="subject">SUBJECT</label>
+            <input type="text" id="subject" name="subject" required></input>
+          </div>
+
+          <label htmlFor="message">MESSAGE</label>
+          <textarea id="message" name="message" required></textarea>
+
+          <button type="submit">SUBMIT</button>
+        </form>
       </footer>
     </main>
   );
