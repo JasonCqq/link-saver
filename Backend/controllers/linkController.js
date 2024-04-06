5afe2e462eedd8f148a6a226f0faaf483f19a3dc
Jason <121495300+jason21715@users.noreply.github.com>
2024-04-06 01:35:00 -0700

    Add UI Update, Add privacy policy

--- Backend/controllers/linkController.js
+++ Backend/controllers/linkController.js
@@ -44,13 +44,43 @@ exports.create_link = [
         }
 
         // Scrape for title/screenshot
-        const browser = await puppeteer.launch({ headless: false });
+        const browser = await puppeteer.launch({
+          headless: false,
+          args: minimal_args,
+        });
         const page = await browser.newPage();
+
+        const blocked_domains = [
+          "googlesyndication.com",
+          "adservice.google.com",
+        ];
+
+        await page.setRequestInterception(true);
+        page.on("request", (request) => {
+          const url = request.url();
+          const isUnnecessaryResource = [
+            "image",
+            "stylesheet",
+            "font",
+          ].includes(request.resourceType());
+          const isBlockedDomain = blocked_domains.some((domain) =>
+            url.includes(domain),
+          );
+
+          if (isBlockedDomain || isUnnecessaryResource) {
+            request.abort();
+          } else {
+            request.continue();
+          }
+        });
 
         await page.goto(decodedUrl, { waitUntil: "domcontentloaded" });
 
         const title = await page.title();
-        const thumbnail = await page.screenshot({ encoding: "base64" });
+        const thumbnail = await page.screenshot({
+          encoding: "base64",
+          fullPage: false,
+        });
 
         await browser.close();
 
@@ -339,3 +369,42 @@ exports.search_link = asyncHandler(async (req, res) => {
 
   res.status(200).json({ links: formattedLinks });
 });
+
+//Puppeteer Settings
+const minimal_args = [
+  "--autoplay-policy=user-gesture-required",
+  "--disable-background-networking",
+  "--disable-background-timer-throttling",
+  "--disable-backgrounding-occluded-windows",
+  "--disable-breakpad",
+  "--disable-client-side-phishing-detection",
+  "--disable-component-update",
+  "--disable-default-apps",
+  "--disable-dev-shm-usage",
+  "--disable-domain-reliability",
+  "--disable-extensions",
+  "--disable-features=AudioServiceOutOfProcess",
+  "--disable-hang-monitor",
+  "--disable-ipc-flooding-protection",
+  "--disable-notifications",
+  "--disable-offer-store-unmasked-wallet-cards",
+  "--disable-popup-blocking",
+  "--disable-print-preview",
+  "--disable-prompt-on-repost",
+  "--disable-renderer-backgrounding",
+  "--disable-setuid-sandbox",
+  "--disable-speech-api",
+  "--disable-sync",
+  "--hide-scrollbars",
+  "--ignore-gpu-blacklist",
+  "--metrics-recording-only",
+  "--mute-audio",
+  "--no-default-browser-check",
+  "--no-first-run",
+  "--no-pings",
+  "--no-sandbox",
+  "--no-zygote",
+  "--password-store=basic",
+  "--use-gl=swiftshader",
+  "--use-mock-keychain",
+];
